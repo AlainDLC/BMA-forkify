11dfdb01109235a4cc11e61616793c20070274e4
AlainDLC <jd_alain@hotmail.com>
2024-11-08 17:18:53 +0100

    fetch some dummy data

---
+++ root/dist/index.62406edb.js
@@ -0,0 +1,38 @@
+const recipeContainer = document.querySelector(".recipe");
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+// https://forkify-api.herokuapp.com/v2
+// 71692436-5517-472d-8a28-b915f6a68772
+// https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza
+// 664c8f193e7aa067e94e89af
+// https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8ad1");
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            tilte: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err.message);
+    }
+};
+showRecipe();
+
+//# sourceMappingURL=index.62406edb.js.map
--- root/src/js/controller.js
+++ root/src/js/controller.js
@@ -9,5 +9,38 @@ const timeout = function (s) {
 };
 
 // https://forkify-api.herokuapp.com/v2
+// 71692436-5517-472d-8a28-b915f6a68772
 
+// https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza
+// 664c8f193e7aa067e94e89af
+// https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8ad1'
+    );
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      tilte: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+showRecipe();
